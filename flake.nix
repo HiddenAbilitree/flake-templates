b5d1e06c270301767c5ea601a9f65d3899877ad4
Eric Zhang <eric@ericzhang.dev>
2024-11-30 07:07:33 -0500

    fix: fixed root flake

--- flake.nix
+++ flake.nix
@@ -1,8 +1,13 @@
 {
   description = "loser flake templates";
 
-  outputs = {
+  outputs = inputs: {
     templates = {
+      test = {
+        path = ./templates/test;
+        description = "A test template";
+      };
+
       bun = {
         path = ./templates/bun;
         description = "A bun template";
--- templates/bun/flake.nix
+++ templates/bun/flake.nix
@@ -1,26 +1,30 @@
 {
+  description = "A bun template";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     systems.url = "github:nix-systems/default";
   };
 
-  outputs =
-    { systems, nixpkgs, ... }@inputs:
-    let
-      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
-    in
-    {
-      devShells = eachSystem (pkgs: {
-        default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.nodejs_23
-            pkgs.bun
-            pkgs.zsh
+  outputs = {
+    self,
+    systems,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+  in {
+    devShells = eachSystem (pkgs: {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.nodejs_23
+          pkgs.bun
+          pkgs.zsh
 
-            pkgs.nodePackages.typescript
-            pkgs.nodePackages.typescript-language-server
-          ];
-        };
-      });
-    };
+          pkgs.nodePackages.typescript
+          pkgs.nodePackages.typescript-language-server
+        ];
+      };
+    });
+  };
 }
